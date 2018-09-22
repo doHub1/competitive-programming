b727f505382481acaa2d368c4123cf35f7907943
endo <whiz777@gmail.com>
2018-09-22 23:48:43 +0900

    CODE FESTIVAL 2018

--- CODE FESTIVAL 2018/b.py
+++ CODE FESTIVAL 2018/b.py
@@ -5,8 +5,27 @@ for i in range(M):
     l,r = map(int,input().split())
     L.append(l)
     R.append(r)
-print(L)
-print(R)
+#print(L)
+#print(R)
+
+
+# 値がB、長さNの配列を用意する
+array = [B] * N
+#print(array)
+
+
+for i in range(M):
+    for j in range(1,N+1):
+        if L[i] <= j and j <= R[i] :
+#            print('replace')
+            array[j-1] = A
+#        else:
+#            print('No')
+#        print('now:', array)
+
+print(sum(array))
+
+
 
 
 
